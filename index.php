b405f2448d25d16e048f45e4d713249ef65f6450
Carlo <carlo.nicolo@hotmail.com>
2026-01-20 22:49:13 +0100

    New version course 2026

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h1>My first PHP page</h1>
+    <h1>My first PHP page</h1>
 
-<?php
-$txt = "PHP";
-echo "I love $txt!";
-?>
+    <?php
+    $txt = "PHP";
+    echo "I love $txt! Yes yes yes!!!";
+    ?>
 
-<ul>
-<li><a href="variables.php">Varibles</a></li>
-<li><a href="variablesScope.php">Variables scope</a></li>
-<li><a href="firstPHPclass.php">First PHP class</a></li>
-<li><a href="functions.php">Functions</a></li>
-<li><a href="postExample.php">Post exercise</a></li>
-<li><a href="automobile.php">1<sup>nd</sup> OOP script Automobile</a></li>
-<li><a href="prodotto.php">2<sup>nd</sup> OOP script Prodotto</a></li>
-</ul>
+    <ul>
+        <li><a href="variables.php">Variables</a></li>
+        <!--
+        <li><a href="variablesScope.php">Variables scope</a></li>
+        <li><a href="firstPHPclass.php">First PHP class</a></li>
+        <li><a href="functions.php">Functions</a></li>
+        <li><a href="postExample.php">Post exercise</a></li>
+        <li><a href="automobile.php">1<sup>nd</sup> OOP script Automobile</a></li>
+        <li><a href="prodotto.php">2<sup>nd</sup> OOP script Prodotto</a></li>
+        -->
+    </ul>
 
 </body>
+
 </html>
--- variables.php
+++ variables.php
@@ -1,42 +1,51 @@
 <!DOCTYPE html>
 <html>
+
 <body>
 
-<h1>PHP variables</h1>
+    <h1>PHP variables</h1>
 
-<?php
+    <?php
 
-$txt = "PHP";
-echo "I love " . $txt . "!" . "<br/>";;
+    $txt = "PHP";
+    $txt1 = "Learn PHP";
+    $txt2 = "5TPS";
 
-$x = 5;      // $x is an integer
-$y = "John"; // $y is a string
-echo $x . "<br/>";
-echo $y . "<br/>";
+    echo "I love " . $txt . "!" . "<br/>";
+    print "<h3>Using print function: $txt1</h3>";
 
-echo "Utilizzo var_dump() function <br/>";
+    print '<h3>' . $txt1 . '</h3>';
+    print '<p>Study PHP at ' . $txt2 . '</p>';
 
-var_dump(5);
-echo "<br/>";
+    $x = 5;      // $x is an integer
+    $y = "John"; // $y is a string
+    echo $x . "<br/>";
+    echo $y . "<br/>";
 
-var_dump("John");
-echo "<br/>";
+    echo "Utilizzo var_dump() function <br/>";
 
-var_dump(3.14);
-echo "<br/>";
+    var_dump(5);
+    echo "<br/>";
 
-var_dump(true);
-echo "<br/>";
+    var_dump("John");
+    echo "<br/>";
 
-var_dump([2, 3, 56]);
-echo "<br/>";
+    var_dump(3.14);
+    echo "<br/>";
 
-var_dump(NULL);
-echo "<br/>";
+    var_dump(true);
+    echo "<br/>";
 
-?>
+    var_dump([2, 3, 56]);
+    echo "<br/>";
 
-<a href="index.php">Home</a>
+    var_dump(NULL);
+    echo "<br/>";
+
+    ?>
+
+    <a href="index.php">Home</a>
 
 </body>
+
 </html>
